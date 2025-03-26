2b641ac0808974cbaa88f6a9b8da569a17a86a2f
NEWY-PC <newy1337@gmail.com>
2025-03-26 12:30:40 +0000

    chatid

--- commands/subscribe.js
+++ commands/subscribe.js
@@ -5,6 +5,7 @@ export async function subscribeCommand(prisma, bot, msg) {
 
     const chatId = msg.chat.id.toString();
 
+    console.log(chatId)
     if (!msg.reply_to_message || !msg.reply_to_message.from) {
         const errorMsg = await  bot.sendMessage(chatId, "Пожалуйста, ответьте на сообщение пользователя, на которого хотите подписаться и укажите /sub");
         await deleteMessages(bot,chatId,[msg.message_id,errorMsg.message_id])
@@ -25,9 +26,9 @@ export async function subscribeCommand(prisma, bot, msg) {
 
         const existingSubscription = await prisma.subscription.findFirst({
             where: {
-                    chatId,
-                    subscriberId: fromUser.id,
-                    targetId: targetUser.id,
+                chatId,
+                subscriberId: fromUser.id,
+                targetId: targetUser.id,
             },
         });
 
